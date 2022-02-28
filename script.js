e5788c1f4eb95d820549c839d7527d420107592c
shdhrubo <shorifulhabib.iit@gmail.com>
2022-02-28 23:10:05 +0600

    search feild value can be showed to console

---
+++ script.js
@@ -0,0 +1,9 @@
+//display searched phone
+const searchPhone=()=>{
+    const searchFeild=document.getElementById('search-feild');
+    const searchFeildValue=searchFeild.value;
+    console.log(searchFeildValue);
+}
+
+
+
