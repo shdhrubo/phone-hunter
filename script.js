87ec5a2e0ce47bfe3624192e0551bfdf436390cc
shdhrubo <shorifulhabib.iit@gmail.com>
2022-02-28 23:44:08 +0600

    phones showed in ui after search

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 //display searched phone
-const searchPhone=()=>{
-    const searchFeild=document.getElementById('search-feild');
-    const searchFeildValue=searchFeild.value;
-    // console.log(searchFeildValue);
-    const url=` https://openapi.programming-hero.com/api/phones?search=${searchFeildValue}`;
-    // console.log(url);
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>console.log(data.data))
-}
-
-
-
-
+const searchPhone = () => {
+  const searchFeild = document.getElementById("search-feild");
+  const searchFeildValue = searchFeild.value;
+  // console.log(searchFeildValue);
+  searchFeild.value = "";
+  const url = ` https://openapi.programming-hero.com/api/phones?search=${searchFeildValue}`;
+  // console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displaySearchedPhone(data.data));
+};
+const displaySearchedPhone = (data) => {
+  // console.log(data);
+  const searchResult = document.getElementById("search-result");
+  searchResult.textContent = "";
+  data.forEach((data) => {
+    const col = document.createElement("div");
+    col.classList.add("col");
+    col.innerHTML = `<div class="col">
+    <div class="card">
+      <img src="${data.image}" class="card-img-top w-50 mt-3 mx-auto" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${data.phone_name}</h5>
+        <h6 class="card-title">Brand : ${data.brand}</h6>
+       <button class="btn btn-warning mt-2">Details</button>
+      </div>
+    </div>
+  </div>`;
+    searchResult.appendChild(col);
+  });
+};
