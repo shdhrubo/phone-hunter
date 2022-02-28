bdbc2bc5cd376ab5dedd87818929e10d2cc7c3ef
shdhrubo <shorifulhabib.iit@gmail.com>
2022-02-28 23:16:44 +0600

    data can be consoled from api using name

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 const searchPhone=()=>{
     const searchFeild=document.getElementById('search-feild');
     const searchFeildValue=searchFeild.value;
-    console.log(searchFeildValue);
+    // console.log(searchFeildValue);
+    const url=` https://openapi.programming-hero.com/api/phones?search=${searchFeildValue}`;
+    // console.log(url);
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>console.log(data.data))
 }
+
 
 
 
